7b1871031f6bf05e9ea7b07c9ee7057486a9374f
pongyuenlam <121112796+pongyuenlam@users.noreply.github.com>
2024-08-04 16:38:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain"});
-    res.end("Hello World!");
+    res.end("Hello - testing");
 });
 
 const PORT = process.env.PORT || 3000;
